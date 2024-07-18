cf72b15d4cabf90270cf75bfd8fb1a790192fe81
bytes <85473667+actuallybytes@users.noreply.github.com>
2024-07-18 07:55:05 +0200

    Update Windows-Activation-Watermark-Remover.py

--- Windows-Activation-Watermark-Remover.py
+++ Windows-Activation-Watermark-Remover.py
@@ -2,6 +2,6 @@ import os
 os.system('title 𝙬𝙞𝙣𝙙𝙤𝙬𝙨 𝙬𝙖𝙩𝙚𝙧𝙢𝙖𝙧𝙠 𝙧𝙚𝙢𝙤𝙫𝙚𝙧')
 os.system('cls')
 
-os.system('reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v PaintDesktopVersion /t REG_DWORD /d 0 /f')
+os.system('reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v PaintDesktopVersion /t REG_DWORD /d 0 /f >nul')
 print(f'Done! Restart your PC to apply the changes.')
 os.system('pause >nul')
